6138612a50caa71fa03d5c94af113d3d5128ed3d
alfredasare <alfredasare101@gmail.com>
2021-09-01 12:25:54 +0000

    Add other svgs

--- components/landing/landingSvg.tsx
+++ components/landing/landingSvg.tsx
@@ -1,4 +1,6 @@
-import Developers from '../svg/landing/developers';
+// import Developers from '../svg/landing/developers';
+// import OutSource from "../svg/landing/outSource";
+import Developer from '../svg/landing/developer';
 
 const LandingSvg = () => {
 	return (
@@ -6,7 +8,7 @@ const LandingSvg = () => {
 			className='flex justify-center items-center mt-12 -mb-16 sm:-mb-48 lg:m-0
 						lg:relative sm:w-4/6 lg:w-full'
 		>
-			<Developers />
+			<Developer />
 		</div>
 	);
 };
