0599cf7cb179da419efabda0de75f7c31df0a4e7
alfredasare <alfredasare101@gmail.com>
2021-09-10 01:32:46 +0000

    Remove unused useRef

--- components/why-us/whyUs.tsx
+++ components/why-us/whyUs.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { motion, useAnimation } from 'framer-motion';
 import { annotate, annotationGroup } from 'rough-notation';
 import { useInView } from 'react-intersection-observer';
